93598ad8d523b676af5f24e4326d990e3d0db292
ergunmu1 <mustafa.ergun@natixis.com>
2021-03-30 17:02:42 +0200

    first commit quete bases JS

---
+++ quete-bases-JS.js
@@ -0,0 +1,12 @@
+/* quete bases JS */
+
+// Define some variables
+const filmName = 'Forrest Gump';
+const movieDate = 1994;
+const directorName = 'Robert Zemeckis';
+
+//const message = filmName, 'réalisé par', directorName, 'est sorti en', movieDate
+
+//const message = 'Hello ' + myName + '! enjoy studying JavaScript at ' + myCampus + '!';
+// Display a popup
+alert(filmName + ' réalisé par '+ directorName+ ' est sorti en '+ movieDate);
